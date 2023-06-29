00acadbfa91be1369baa4cc29621b78fbe917b80
Cuong Nguyen <cuongvnus2001@gmail.com>
2023-06-28 22:32:48 -0700

    Clean up code, comments, remove date asking

--- plotspectrum.py
+++ plotspectrum.py
@@ -5,26 +5,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import digital_rf as drf
-import maidenhead
-from datetime import datetime
-import datetime
+import maidenhead, time, datetime, math
 from datetime import timezone
-import math
-import os, tempfile
+
+currTime = time.time()
 
 plt.style.use('_mpl-gallery-nogrid')
-maidenheadGrid = 'EN91'
-# plot
-fig, ax = plt.subplots()
+maidenheadGrid = 'EN91'                     # init maidenheadGrid variable
+
 dataDir = "OBS2023-06-26"
 metadata_dir = dataDir + '\ch0\metadata'
 
 do = drf.DigitalRFReader(dataDir)
 s, e = do.get_bounds('ch0')
 
-
-print("Plot spectrum for what date?  (YYYY-MM-DD)")
-t =input()
+t = dataDir[3:13]
+print("Plotting spectrum for " + t)
 t = t + "T00:00"
 requestTime = datetime.datetime.strptime(t, '%Y-%m-%dT%H:%M')
 # these are based on unix time * 10 (for 10 samples/sec)
@@ -37,12 +33,10 @@ freqList = [0]
 theLatitude = 0
 theLongitude = 0
 
-# get Metadata, if it exists
+# ---------------------------------------
+# Get metadata
 try:
     dmr = drf.DigitalMetadataReader(metadata_dir)
-    print("metadata init okay")
-  #  start_idx = int(np.uint64(stime * dmr.get_samples_per_second()))
-   # start_idx = int(np.uint64(s)) # test
     first_sample, last_sample = dmr.get_bounds()
     print("metadata bounds are %i to %i" % (first_sample, last_sample))
 
@@ -55,19 +49,16 @@ try:
     print("first read - just get one column ")
     data_dict = dmr.read(start_idx, start_idx + 2, "center_frequencies")
     for key in data_dict.keys():
-      #  print((key, data_dict[key]))
         freqList = data_dict[key]
         print("freq = ",freqList[0])
 
     data_dict = dmr.read(start_idx, start_idx + 2, "lat")
     for key in data_dict.keys():
-     #   print((key, data_dict[key]))
         theLatitude = data_dict[key]
         print("Latitude: ",theLatitude)
         
     data_dict = dmr.read(start_idx, start_idx + 2, "long")
     for key in data_dict.keys():
-      #  print((key, data_dict[key]))
         theLongitude = data_dict[key]
         print("Longitude: ",theLongitude)
 
@@ -76,10 +67,10 @@ try:
 except IOError:
     print("IO Error; metadata not found at " + metadata_dir)
 
-
+# ----------------------------------------
+# Read data
 gain = 1.5
 hr1 = np.arange(1024, dtype='f')
-#print("numpy array type = ",type(hr1[0]))
 zeros = np.zeros(1024, dtype='f')
 
 print('Read data... this may take a few minutes...')
@@ -102,7 +93,6 @@ for i in range(1439): # 1439 gives 1440 bins
         else:
             Q = np.append(Q, [hr1], axis=0)
     except IOError: # tried to read DRF data but did not find requsted time slice
-      #  print("data gap at ", s + offset)
         if (offset == 0):
             Q = np.array([zeros])
         else:
@@ -114,22 +104,19 @@ for i in range(1439): # 1439 gives 1440 bins
         print(".",end='')
 
     Q[i,1022] = 0 # blank out bottom bin
-    
-#print('Q type=',type(Q), " size=", Q.size)
-print("File loaded")
-fig, ax = plt.subplots(2,1, figsize=(14,8)) # plot size, inches x and y
 
-#fig, ax = plt.subplots(3,1, figsize=(14,10)) # plot size, inches x and y
+print("File loaded")
+
+print(time.time()-currTime)
+
+# -----------------------------------------------
+# Plot
+
+fig, ax = plt.subplots(2, 1, figsize=(14,8)) # plot size, inches x and y
 
 intensity = 1.5 # multiplier to intensify spectrum display
 P = intensity * np.absolute(Q.transpose(1, 0)) # re-orient to show horizontal waterfall
 Q1 = np.absolute(Q)
-
-# can use get_cmap('jet') also viridis, jet, hot
-#ax.imshow(P, cmap =plt.get_cmap('hot'), interpolation ='nearest', vmin=0,vmax=1)
-
-#P1 = P # get copy of this input matrix
-#R = np.zeros((1440, 1024))
 
 D = np.zeros((1024,1440)) # derivative
 D1 = np.zeros((1024, 1440))
@@ -142,27 +129,18 @@ for column in range(0, 1439):  # iterate thru minutes
     # here we skip topmost 1 or 2 bins as they contiain only the DC offet
     for bin in range(1, 1020): # iterate thru bins of this minute
         if Q1[column, bin] > maxval:
-           # print("col bin val ", column, bin, Q1[column, bin])
             maxval = Q1[ column, bin ]
             maxptr = bin  # remember which bin had highest amplitude
         D[bin, column] = 5 * (P[bin, column] - P[bin-1, column])
         D1[bin, column] = 5 * (D[bin, column] - D[bin-1, column])
 
-    #    D[column, bin] = Q[column, bin] - Q[column, bin+1]
-    #print("max bin at col", column, bin, maxptr, " value ", maxval)
-   # z = input()
     M[column] = maxptr  # remember max bin for each column
     amplitudes[column] = maxval
-    
-#R2 = np.absolute(R.transpose(1, 0))
+
 avgAmplitude = np.average(amplitudes, weights=None)
 stdDevFreq = np.std(M[0:840])
 print("First 14 hours STD DEV = ",stdDevFreq)
 
-# scale intensity of waterfall according to average signal amplitude
-# vmax = 100 also can be used
-
-#ax[0].imshow(P, cmap =plt.get_cmap('inferno'), interpolation ='nearest', vmin=0,vmax=(avgAmplitude*1.5))
 ax[0].text(40,40,'std.dev. = ' + str(stdDevFreq),color='white')
 
 # original cmap was gist_ncar
@@ -180,32 +158,26 @@ plt.scatter(x_points, y_points, c='red', marker = "+", s=1) # s is is marker siz
 
 ax[0].set_aspect('auto')
 
-#locs, labels = yticks()
-
 plt.yticks(np.arange(0,1100,100), labels=['5','4','3','2','1','0','-1','-2','-3','-4','-5'])
 plt.xticks(np.arange(0,1560, 120), labels=['00','02','04','06','08','10','12','14','16','18','20','22','24'])
 ax[0].set_ylabel('Hz')
 ax[0].set_xlabel('Hours, UTC')
-#ax[0].set_title('Grape Narrow Spectrum, Freq. = ' + str(freqList[0]) + " MHz, " + t + ' , Grid ' + maidenheadGrid)
 ax[0].set_title('Grape Narrow Spectrum, Freq. = ' + str(freqList[0]) + " MHz, " + t + ' , Lat. '
                 + '{:6.2f}'.format(theLatitude) + ", Long. " + '{:6.2f}'.format(theLongitude) + ' (Grid'
                 + maidenheadGrid + ')')
 plt.subplot(212)
-#plt.subplot(312)
 plt.margins(x=0)
-#plt.gca().set_ylim(top=100)  doesn;t work
 plt.autoscale(enable=True,axis='y')
 ax[1].plot(amplitudes)
 
 plt.xticks(np.arange(0,1560, 120), labels=['00','02','04','06','08','10','12','14','16','18','20','22','24'])
-#plt.yticks(np.arange(0,1000, 200), labels=['0','25','50','75','100'])
 ax[1].set_ylabel('Amplitude, uncalibrated units')
 ax[1].set_xlabel('Hours, UTC')
 ax[1].set_title('Peak Amplitude by Minute' )
 fig.tight_layout()
 print("Average amplitude: ",avgAmplitude)
-#plt.subplot(3,1,3)
-#ax[2].imshow(D, cmap =plt.get_cmap('inferno'), interpolation ='nearest', vmin=0,vmax=(avgAmplitude*1.5))
 
-plt.savefig('images/DRF_plots/' + t + '_' + maidenheadGrid + '.jpg')
+print(time.time()-currTime)
+
+plt.savefig('images\DRF_plots\\' + t + '_' + maidenheadGrid + '.jpg')
 plt.show()
